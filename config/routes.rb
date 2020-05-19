931312c6bf9722c1d299bc3ff7bbe2bde37e25a3
Angela Nguyen <angethuy@gmail.com>
2020-05-19 01:58:07 -0700

    initial scaffolding for ice cream, votes, and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :ice_creams
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
