8d6d47e06182bd355deb3fcee9c3c2fa928a983d
Angela Nguyen <angethuy@gmail.com>
2020-05-22 20:59:56 -0700

    nominal vote model tests passing

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,11 +1,22 @@
 require "test_helper"
 
 describe Vote do
+
+  before do
+    @ice_cream = ice_creams(:classic)
+    @user = users(:elvy)
+    @vote = Vote.create(ice_cream_id: @ice_cream.id, user_id: @user.id, value: 1)
+  end
+
   describe "relations" do
     it "has a user" do
+      expect(@vote.respond_to?(:user)).must_equal true
+      expect(@vote.user_id).must_equal @user.id
     end
 
     it "has an ice cream" do
+      expect(@vote.respond_to?(:ice_cream)).must_equal true
+      expect(@vote.ice_cream_id).must_equal @ice_cream.id
     end
   end
 end
