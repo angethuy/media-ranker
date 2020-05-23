0850987a45a6cffc5bb44a38c2e4c36184523b74
Angela Nguyen <angethuy@gmail.com>
2020-05-22 20:39:32 -0700

    ice_cream controller tests failing predictably

--- test/controllers/ice_creams_controller_test.rb
+++ test/controllers/ice_creams_controller_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 describe IceCreamsController do
-  let(:ice_cream) { ice_creams(:one) }
+
+  before do
+    @ice_cream = ice_creams(:strawberry)
+  end
 
   it "should get index" do
     get ice_creams_url
@@ -27,25 +30,28 @@ describe IceCreamsController do
     it "should not create duplicated ice_cream, and flash danger" do
       duplicated_ice_cream = IceCream.last
       assert_no_difference("IceCream.count") do
-        post ice_creams_url, params: { ice_cream: { base_flavor: @duplicated_ice_cream.base_flavor, brand: @duplicated_ice_cream.brand, category: @duplicated_ice_cream.category, description: @duplicated_ice_cream.description, name: @duplicated_ice_cream.name }}
+        post ice_creams_url, params: { ice_cream: { base_flavor: duplicated_ice_cream.base_flavor, brand: duplicated_ice_cream.brand, category: duplicated_ice_cream.category, description: duplicated_ice_cream.description, name: duplicated_ice_cream.name }}
       end
 
       must_redirect_to new_ice_cream_url
       assert_equal "Ice cream already exists, please add a new one.", flash[:danger]
     end
 
-    it "should redirect to new form and flash danger for ice creams with no name" do
-      let(:yucky_ice_cream) {
-        ice_cream: {
-          name: nil,
-          category: "scoop",
-          brand: "How The Grinch Makes Ice Cream",
-          base_flavor: "mushrooms",
-          description: "you're a weird one, mister grinch"
-        }
+    let(:yucky_ice_cream) { 
+      {
+      ice_cream: {
+        name: nil,
+        category: "scoop",
+        brand: "How The Grinch Makes Ice Cream",
+        base_flavor: "mushrooms",
+        description: "you're a weird one, mister grinch"
       }
-      assert_no_difference "IceCream.count" do
-        post ice_creams_url, params: { :yucky_ice_cream }
+    }
+  }
+
+    it "should redirect to new form and flash danger for ice creams with no name" do
+      assert_no_difference("IceCream.count") do
+        post ice_creams_url, params: yucky_ice_cream
       end
       must_redirect_to new_ice_cream_url
       assert_equal "Please fill in missing ice cream name", flash[:danger]
@@ -58,32 +64,32 @@ describe IceCreamsController do
   end
 
   it "should get edit" do
-    get edit_ice_cream_url(@ice_cream)
+    get edit_ice_cream_url(@ice_cream.id)
     must_respond_with :success
   end
 
   describe "update" do
     it "should update ice_cream when given valid params" do
-      patch ice_cream_url(@ice_cream), params: { ice_cream: { base_flavor: @ice_cream.base_flavor, brand: @ice_cream.brand, category: @ice_cream.category, description: @ice_cream.description, name: @ice_cream.name } }
-      must_redirect_to ice_cream_url(ice_cream)
+      patch ice_cream_url(@ice_cream.id), params: { ice_cream: { base_flavor: @ice_cream.base_flavor, brand: @ice_cream.brand, category: @ice_cream.category, description: @ice_cream.description, name: @ice_cream.name } }
+      must_redirect_to ice_cream_url(@ice_cream.id)
       assert_equal "<a href=\"/ice_creams/#{@ice_cream.id}\"> successfully updated.", flash[:success]
     end
 
     it "should not update ice_cream when given invalid params" do
-      assert_no_difference (@ice_cream)
+      assert_no_difference("IceCream.count") do
         patch ice_cream_url(@ice_cream), params: { ice_cream: { base_flavor: @ice_cream.base_flavor, brand: @ice_cream.brand, category: @ice_cream.category, description: @ice_cream.description, name: nil } }
       end
       must_redirect_to edit_ice_cream_url
-      assert_equal "Could not edit <a href=\"/ice_creams/#{@ice_cream.id}\">, missing name."
+      assert_equal "Could not edit <a href=\"/ice_creams/#{@ice_cream.id}\">, missing name.", flash[:danger]
     end
   end
 
   it "should destroy ice_cream" do
     assert_difference("IceCream.count", -1) do
-      delete ice_cream_url(@ice_cream)
+      delete ice_cream_url(@ice_cream.id)
     end
 
     must_redirect_to ice_creams_url
-    assert_equal "Successfully deleted ice cream."
+    assert_equal "Successfully deleted ice cream.", flash[:success]
   end
 end
