73f32a3907f7add5928198f918790adc97265f5d
Angela Nguyen <angethuy@gmail.com>
2020-05-22 20:19:44 -0700

    added testing for ice_creams controller and model

--- test/controllers/ice_creams_controller_test.rb
+++ test/controllers/ice_creams_controller_test.rb
@@ -13,12 +13,43 @@ describe IceCreamsController do
     must_respond_with :success
   end
 
-  it "should create ice_cream" do
-    assert_difference("IceCream.count") do
-      post ice_creams_url, params: { ice_cream: { base_flavor: @ice_cream.base_flavor, brand: @ice_cream.brand, category: @ice_cream.category, description: @ice_cream.description, name: @ice_cream.name } }
+  describe "create" do
+
+    it "should create ice_cream and flash success for valid forms" do
+      assert_difference("IceCream.count") do
+        post ice_creams_url, params: { ice_cream: { base_flavor: @ice_cream.base_flavor, brand: @ice_cream.brand, category: @ice_cream.category, description: @ice_cream.description, name: @ice_cream.name } }
+      end
+
+      must_redirect_to ice_cream_url(IceCream.last)
+      assert_equal "Successfully added new Ice Cream: <a href=\"/ice_creams/#{@ice_cream.id}\">#{@ice_cream.name}</a>", flash[:success]
     end
 
-    must_redirect_to ice_cream_url(IceCream.last)
+    it "should not create duplicated ice_cream, and flash danger" do
+      duplicated_ice_cream = IceCream.last
+      assert_no_difference("IceCream.count") do
+        post ice_creams_url, params: { ice_cream: { base_flavor: @duplicated_ice_cream.base_flavor, brand: @duplicated_ice_cream.brand, category: @duplicated_ice_cream.category, description: @duplicated_ice_cream.description, name: @duplicated_ice_cream.name }}
+      end
+
+      must_redirect_to new_ice_cream_url
+      assert_equal "Ice cream already exists, please add a new one.", flash[:danger]
+    end
+
+    it "should redirect to new form and flash danger for ice creams with no name" do
+      let(:yucky_ice_cream) {
+        ice_cream: {
+          name: nil,
+          category: "scoop",
+          brand: "How The Grinch Makes Ice Cream",
+          base_flavor: "mushrooms",
+          description: "you're a weird one, mister grinch"
+        }
+      }
+      assert_no_difference "IceCream.count" do
+        post ice_creams_url, params: { :yucky_ice_cream }
+      end
+      must_redirect_to new_ice_cream_url
+      assert_equal "Please fill in missing ice cream name", flash[:danger]
+    end
   end
 
   it "should show ice_cream" do
@@ -31,9 +62,20 @@ describe IceCreamsController do
     must_respond_with :success
   end
 
-  it "should update ice_cream" do
-    patch ice_cream_url(@ice_cream), params: { ice_cream: { base_flavor: @ice_cream.base_flavor, brand: @ice_cream.brand, category: @ice_cream.category, description: @ice_cream.description, name: @ice_cream.name } }
-    must_redirect_to ice_cream_url(ice_cream)
+  describe "update" do
+    it "should update ice_cream when given valid params" do
+      patch ice_cream_url(@ice_cream), params: { ice_cream: { base_flavor: @ice_cream.base_flavor, brand: @ice_cream.brand, category: @ice_cream.category, description: @ice_cream.description, name: @ice_cream.name } }
+      must_redirect_to ice_cream_url(ice_cream)
+      assert_equal "<a href=\"/ice_creams/#{@ice_cream.id}\"> successfully updated.", flash[:success]
+    end
+
+    it "should not update ice_cream when given invalid params" do
+      assert_no_difference (@ice_cream)
+        patch ice_cream_url(@ice_cream), params: { ice_cream: { base_flavor: @ice_cream.base_flavor, brand: @ice_cream.brand, category: @ice_cream.category, description: @ice_cream.description, name: nil } }
+      end
+      must_redirect_to edit_ice_cream_url
+      assert_equal "Could not edit <a href=\"/ice_creams/#{@ice_cream.id}\">, missing name."
+    end
   end
 
   it "should destroy ice_cream" do
@@ -42,5 +84,6 @@ describe IceCreamsController do
     end
 
     must_redirect_to ice_creams_url
+    assert_equal "Successfully deleted ice cream."
   end
 end
--- test/models/ice_cream_test.rb
+++ test/models/ice_cream_test.rb
@@ -2,26 +2,63 @@ require "test_helper"
 
 describe IceCream do
   describe "validations" do
+    before do 
+      user = users(:picchu)
+      @ice_cream = IceCream.new(name: "Salted Caramel Truffle", category: "scoop", brand: "Haagen Dazs", base_flavor: "caramel", description: "salted caramel base with caramel swirls and the cutest tiny truffles")
+
+    end
     it "is valid when category and name are present" do
+      expect(@ice_cream.valid?).must_equal true
     end
 
     it "is invalid when there is no ice cream name" do
+      @ice_cream.name = nil
+      expect(@ice_cream.valid?).must_equal false
+      expect(@ice_cream.errors.messages).must_include :name
     end
 
     it "is invalid when category and ice cream name are non-unique" do
+      unique_ice_cream = IceCream.create!(name: "Picchu's Kibbles n Carrots", category: "scoop", brand: "Picchu", base_flavor: "kibble", description: "it tastes like frozen kibbles")
+      @ice_cream.name = IceCream.last.name
+      expect(@ice_cream.valid?).must_equal false
+      expect(@ice_cream.errors.messages).must_include :name
+    end
+
+    it "is valid when ice cream name is non-unique but category differs" do
+      same_name_ice_cream = IceCream.create!(name: @ice_cream.name, category: "shape", brand: @ice_cream.brand, base_flavor: @ice_cream.base_flavor, description: @ice_cream.description)
+      expect(same_name_ice_cream.name).must_equal @ice_cream.name
+      expect(same_name_ice_cream.category).wont_equal @ice_cream.category
+      expect(same_name_ice_cream.valid?).must_equal true
     end
   end
 
   describe "custom methods" do
     it "retrieves all records matching a given category filter" do
+      category = "scoop" 
+      result = IceCream.get_all_in_category(category)
+      expect(result.length).must_equal 1
+      expect(result.first.category).must_equal category
     end
   end
 
   describe "relations" do
-    it "has votes" do 
+    before do
+      @ice_cream = ice_creams(:spumoni)
+      @user = users(:picchu)
+      vote = Vote.create(ice_cream_id: @ice_cream.id, user_id: @user.id)
+      
     end
 
-    it "has users through votes" do
+    it "has/responds to votes" do 
+      expect(@ice_cream.respond_to?(:votes)).must_equal true
+      expect(@ice_cream.votes.count).must_equal 1
+    end
+
+    it "has/responds to users through votes" do
+      expect(@ice_cream.respond_to?(:users)).must_equal true
+      expect(@ice_cream.users.count).must_equal 1
+      Vote.create(ice_cream_id: @ice_cream.id, user_id: @user.id)
+      expect(@ice_cream.users.count).must_equal 2
     end
   end
 end
