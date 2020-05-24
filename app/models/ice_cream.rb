f62877a4e9cfd9d0940cce7339dc20921e46723b
Angela Nguyen <angethuy@gmail.com>
2020-05-24 01:42:53 -0700

    index page now displaying categories

--- app/models/ice_cream.rb
+++ app/models/ice_cream.rb
@@ -1,4 +1,13 @@
 class IceCream < ApplicationRecord
   has_many :votes
   has_many :users, through: :votes
+
+  CATEGORIES =  %w[scoop shape stick]
+
+  scope :top_within_category, -> (category) { 
+    where(category: category)
+    .order("name ASC") #currently ordered by name
+    # .where(COUNT(votes) > 0)
+  }
+
 end
