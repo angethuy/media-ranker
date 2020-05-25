eb1ee82df55eced8e5ad3c771e0270ce95668777
Angela Nguyen <angethuy@gmail.com>
2020-05-25 02:13:17 -0700

    removed create function from votes controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,16 +1,3 @@
 class VotesController < ApplicationController
-  before_action :set_user, only: [:create]
 
-  def create
-  end
-
-  private
-
-  def set_user
-    @user = session[:user[:id]]
-    if @user = nil
-      #redirect to the referrer with 
-      #fail flash
-    end
-  end
 end
