5d8963e77dffdbf21a2892fafd8affcc84e612f7
Angela Nguyen <angethuy@gmail.com>
2020-05-25 02:12:44 -0700

    fixed routing for voting function

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   root to: "pages#index"
   get 'login', to: 'pages#login'
 
-  resources :ice_creams
+  resources :ice_creams do
+    post 'vote', on: :member
+  end
+
   resources :users, except: [:edit, :new, :update, :destroy]
   resources :votes, only: [:create]
   
