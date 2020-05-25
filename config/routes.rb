8506755885a18dece39824f84bf3575e8e3e14a8
Angela Nguyen <angethuy@gmail.com>
2020-05-25 03:24:14 -0700

    added login, logout, current user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
   root to: "pages#index"
-  get 'login', to: 'pages#login'
-  post 'login', to: 'user#login_as_user'
+  get '/login', to: 'users#login_form', as: "login"
+  post '/login', to: 'users#login'
+  post '/logout', to: 'users#logout', as: "logout"
+  get '/users/current', to: 'users#current', as: 'current_user'
 
   resources :ice_creams do
     post 'vote', on: :member
