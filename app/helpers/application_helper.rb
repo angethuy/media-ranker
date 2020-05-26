ba94e976cc4e825b4cb2addef6b1fe97ca188e20
Angela Nguyen <angethuy@gmail.com>
2020-05-25 22:44:33 -0700

    view helper to create login or logout button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def auth_button(user)
+    if user
+      return content_tag(:a, "Logout", class: 'btn btn-primary', href: '/logout', data: { method: "post"})
+    else 
+      return content_tag(:a, "Login", class: 'btn btn-primary', href: '/login')
+    end
+  end
+
 end
+
+
