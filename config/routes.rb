8f6802c26456bbeff5839dd073e5d6675897327c
Angela Nguyen <angethuy@gmail.com>
2020-05-25 20:27:20 -0700

    removed current user action

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   get '/login', to: 'users#login_form', as: "login"
   post '/login', to: 'users#login'
   post '/logout', to: 'users#logout', as: "logout"
-  get '/users/current', to: 'users#current', as: 'current_user'
 
   resources :ice_creams do
     post 'vote', on: :member
