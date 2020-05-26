a31e02a2863ea89667d53e5ee9c95cdd3009465b
Angela Nguyen <angethuy@gmail.com>
2020-05-25 23:37:08 -0700

    buttons now displaying correctly based on session

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,15 @@ module ApplicationHelper
 
   def auth_button(user)
     if user
-      return content_tag(:a, "Logout", class: 'btn btn-primary', href: '/logout', data: { method: "post"})
+      return content_tag(:a, "Logout", class: 'btn btn-primary', href: '/logout', rel: "nofollow", data: { method: "post"})
     else 
       return content_tag(:a, "Login", class: 'btn btn-primary', href: '/login')
     end
   end
 
+  def profile_button(user)
+    return content_tag(:a, "Logged in as #{user.name}", class: 'btn btn-primary', href: user_path(user.id))
+  end
 end
 
 
