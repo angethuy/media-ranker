ea85f871240fe8c90dbfd88156037255a5a7b440
Angela Nguyen <angethuy@gmail.com>
2020-05-26 01:14:11 -0700

    added model validations

--- app/models/ice_cream.rb
+++ app/models/ice_cream.rb
@@ -1,8 +1,16 @@
 class IceCream < ApplicationRecord
+  
   has_many :votes
   has_many :users, through: :votes
 
   CATEGORIES =  %w[scoop shape stick]
+
+  validates :name, presence: true, uniqueness: {scope: :category, 
+    message: "%{value} already exists in that category." }
+  validates :category, presence: true, inclusion: { in: CATEGORIES,
+    message: "%{value} is not a valid category" }
+
+    
 
   scope :top_within_category, -> (category) { 
     where(category: category)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :votes
   has_many :ice_creams, through: :votes
+
+  validates :name, presence: true, uniqueness: true
+
 end
