27e401e61354ff352baf0dc4ad6e3c12483d0809
Angela Nguyen <angethuy@gmail.com>
2020-05-26 05:02:31 -0700

    added destroy dependencies to delete associated votes when a parent is deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :votes
+  has_many :votes, dependent: :destroy
   has_many :ice_creams, through: :votes
 
   validates :name, presence: true, uniqueness: true
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,13 @@
 class Vote < ApplicationRecord
   belongs_to :ice_cream, counter_cache: true
   belongs_to :user
+
+  def get_user
+    return User.find_by(id: user_id)
+  end
+
+  def get_ice_cream
+    ice_cream = IceCream.find_by(id: ice_cream_id)
+    return ice_cream ? ice_cream : "nil"
+  end
 end
